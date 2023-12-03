6d354bb0f84611bfa92c691edd76f1a43ecddd11
Nicolas Gimenez <59981635+nicolasegimenez@users.noreply.github.com>
2023-12-03 16:45:23 +0000

    add submits

---
+++ mooc-data-analysis-with-python-2023-2024/part01-e04_multiplication_table/src/multiplication_table.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+def main():
+    pass
+    for i in range(1, 11):
+        print('')
+        for f in range (1, 11):
+            print(f'{f*i}\t', end="")
+
+    print(end="")     
+   
+if __name__ == "__main__":
+    main()

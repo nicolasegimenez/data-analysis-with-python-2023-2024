bdd232a1e93a6c830dbca4b063f8bebd2e30bb06
Nicolas Gimenez <59981635+nicolasegimenez@users.noreply.github.com>
2023-12-04 16:00:14 +0000

    add changes

--- mooc-data-analysis-with-python-2023-2024/part01-e05_two_dice/src/two_dice.py
+++ mooc-data-analysis-with-python-2023-2024/part01-e05_two_dice/src/two_dice.py
@@ -2,6 +2,11 @@
 
 def main():
     pass
+    l=[1,2,3,4,5,6]
+    for x in l:
+        for f in range (1,7):
+            if x + f == 5:
+             print(f'({x},{f})')
 
 if __name__ == "__main__":
     main()

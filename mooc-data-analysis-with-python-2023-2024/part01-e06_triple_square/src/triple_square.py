5683750a055ad287475aa908f459baa848a53911
Nicolas Gimenez <59981635+nicolasegimenez@users.noreply.github.com>
2023-12-04 23:33:30 +0000

    triple_square

--- mooc-data-analysis-with-python-2023-2024/part01-e06_triple_square/src/triple_square.py
+++ mooc-data-analysis-with-python-2023-2024/part01-e06_triple_square/src/triple_square.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
-
+def triple(a):
+    return a*3
+def square(a):
+    return a**2
 
 def main():
     pass
+    for i in range(1,11):
+        triple_var = triple(i)
+        square_var = square(i)
+        if square_var > triple_var:
+         break
+        print(f'triple({i})=={triple_var} square({i})=={square_var}')
 
 if __name__ == "__main__":
     main()

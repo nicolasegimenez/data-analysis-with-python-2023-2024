7f78d8da2f37f12dff01312125df3b9b9404fb31
Nicolas Gimenez <59981635+nicolasegimenez@users.noreply.github.com>
2023-12-05 21:17:17 +0000

    update

--- mooc-data-analysis-with-python-2023-2024/part01-e07_areas_of_shapes/src/areas_of_shapes.py
+++ mooc-data-analysis-with-python-2023-2024/part01-e07_areas_of_shapes/src/areas_of_shapes.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 
 import math
-
-
+def triangle_area(base, height):
+    "this function return the area of a triangle"
+    return (base*height)/2
+def rectangle_area(base, height):
+    return(base*height)
+def circle_area(r):
+    return (math.pi*r**2)
+    
 def main():
+    print(circle_area())
     # enter you solution here
 
 if __name__ == "__main__":
